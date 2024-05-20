d785aca928c8f09ee3069f96931d2489e96ac425
Ezequiel Martinez Lopez <zenitram.e14@gmail.com>
2024-05-19 22:40:32 -0300

    modicacion de las clases a modo de prueba.

---
+++ src/Estructura/Cubo.java
@@ -0,0 +1,10 @@
+package Estructura;
+
+import java.util.Map;
+
+import DataClases.Tabla;
+
+public class Cubo {
+    private Map<String, Dimension> dimensiones;
+    private Tabla hechos;
+}
---
+++ src/Estructura/Dimension.java
@@ -0,0 +1,19 @@
+package Estructura;
+
+import java.util.List;
+import java.util.Map;
+
+import DataClases.Tabla;
+
+public class Dimension {
+    private String nombre;
+    private Tabla tabla;
+    
+    public Dimension() {
+        //constructor vacio
+
+    }
+    public Dimension(Tabla tabla, String nombreDimension, Map<String, String> tipos) {
+        
+    }
+}
